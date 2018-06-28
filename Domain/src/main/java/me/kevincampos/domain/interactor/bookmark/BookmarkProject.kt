c88e73f54e1d678b8bff7341562b7df8ad104a62
Kevin Campos <4038983+kevinmcampos@users.noreply.github.com>
2018-06-28 09:54:57 +0200

    Create use cases to get projects, bookmarked projects, do bookmark and unbookmark

---
+++ Domain/src/main/java/me/kevincampos/domain/interactor/bookmark/BookmarkProject.kt
@@ -0,0 +1,26 @@
+package me.kevincampos.domain.interactor.bookmark
+
+import io.reactivex.Completable
+import me.kevincampos.domain.executor.PostExecutionThread
+import me.kevincampos.domain.interactor.CompletableUseCase
+import me.kevincampos.domain.repository.ProjectsRepository
+import javax.inject.Inject
+
+class BookmarkProject @Inject constructor(
+        private val projectsRepository: ProjectsRepository,
+        postExecutionThread: PostExecutionThread
+) : CompletableUseCase<BookmarkProject.Params>(postExecutionThread) {
+
+    override fun buildUseCaseCompletable(params: Params?): Completable {
+        if (params == null) throw IllegalArgumentException("Params can't be null")
+        return projectsRepository.bookmarkProject(params.projectId)
+    }
+
+    data class Params constructor(val projectId: String) {
+        companion object {
+            fun forProject(projectId: String): Params {
+                return Params(projectId)
+            }
+        }
+    }
+}
---
+++ Domain/src/main/java/me/kevincampos/domain/interactor/bookmark/GetBookmarkedProjects.kt
@@ -0,0 +1,18 @@
+package me.kevincampos.domain.interactor.bookmark
+
+import io.reactivex.Observable
+import me.kevincampos.domain.executor.PostExecutionThread
+import me.kevincampos.domain.interactor.ObservableUseCase
+import me.kevincampos.domain.model.Project
+import me.kevincampos.domain.repository.ProjectsRepository
+import javax.inject.Inject
+
+class GetBookmarkedProjects @Inject constructor(
+        private val projectsRepository: ProjectsRepository,
+        postExecutionThread: PostExecutionThread
+) : ObservableUseCase<List<Project>, Nothing>(postExecutionThread) {
+
+    override fun buildUseCaseObservable(params: Nothing?): Observable<List<Project>> {
+        return projectsRepository.getBookmarkedProjects()
+    }
+}
